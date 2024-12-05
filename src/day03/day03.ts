deb98ad85a8a820d9aca08cb448842fb79ee6552
Max Davidson <davidsonmax@gmail.com>
2024-12-05 12:42:10 +0100

    Fix regex in day 03

--- src/day03/day03.ts
+++ src/day03/day03.ts
@@ -9,10 +9,10 @@ export function part2(input: string): number {
   let enabled = true;
   let sum = 0;
 
-  for (const match of input.matchAll(/mul\((\d+),(\d+)\)|do(?:n't)?/g)) {
-    if (match[0] === "do") {
+  for (const match of input.matchAll(/mul\((\d+),(\d+)\)|do\(\)|don't\(\)/g)) {
+    if (match[0] === "do()") {
       enabled = true;
-    } else if (match[0] === "don't") {
+    } else if (match[0] === "don't()") {
       enabled = false;
     } else if (enabled) {
       sum += Number(match[1]) * Number(match[2]);
